c738e87f7f243d581466da23ee352c48072e6fe4
hulkmania40 <ali.mubashir40@gmail.com>
2025-09-14 00:41:37 +0530

    add env variable

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,10 +1,10 @@
 import axios, { type AxiosRequestConfig } from "axios";
 
-const API_BASE_URL = "http://localhost:8000"; // change to your backend url
+const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Create axios instance
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
