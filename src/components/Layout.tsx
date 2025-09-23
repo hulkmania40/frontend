1cbcea1cecfe4b03db4ef3c6c82030f7dd134349
hulkmania40 <ali.mubashir40@gmail.com>
2025-09-23 20:55:54 +0530

    cleanup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router"
+import { Outlet, useNavigate } from "react-router"
 import {
   Sidebar,
   SidebarContent,
@@ -13,6 +13,9 @@ import { CustomNavigationMenu } from "./CustomNavigationMenu"
 import { Separator } from "@/components/ui/separator"
 
 export default function Layout() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="grid grid-cols-[1fr_2fr_6fr_2fr_1fr] h-screen">
       {/* Left Sidebar */}
@@ -21,20 +24,38 @@ export default function Layout() {
           <SidebarGroup>
             <SidebarGroupLabel>Menu</SidebarGroupLabel>
             <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
+              <SidebarMenuItem
+                onClick={()=>{
+                  navigate("/dashboard")
+                }}
+              >
+                <SidebarMenuButton
+                  className="cursor-pointer"
+                >
                   <Home className="mr-2 h-4 w-4" />
                   Dashboard
                 </SidebarMenuButton>
               </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
+              <SidebarMenuItem
+                onClick={()=>{
+                  navigate("/inventory")
+                }}
+              >
+                <SidebarMenuButton
+                  className="cursor-pointer"
+                >
                   <ClipboardList className="mr-2 h-4 w-4" />
                   Inventory
                 </SidebarMenuButton>
               </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
+              <SidebarMenuItem
+                onClick={()=>{
+                  navigate("/admin")
+                }}
+              >
+                <SidebarMenuButton
+                  className="cursor-pointer"
+                >
                   <Shield className="mr-2 h-4 w-4" />
                   Admin
                 </SidebarMenuButton>
